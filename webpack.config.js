baf73fa83dcbcba31b038c86efb4c9a6a9b70560
萧泳森 <xiaoyongsen@tanzhou.cn>
2020-04-27 11:09:12 +0800

    feat: 使用 babel-plugin-import 按需引入 antd 组件

--- webpack.config.js
+++ webpack.config.js
@@ -45,6 +45,21 @@ module.exports = {
             }
           }
         ]
+      },
+      {
+        test: /\.less$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'less-loader',
+            options: {
+              lessOptions: {
+                javascriptEnabled: true
+              }
+            }
+          }
+        ]
       }
     ]
   }
